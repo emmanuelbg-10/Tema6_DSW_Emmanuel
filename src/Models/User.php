d9c9f9c4feebb07ac435c6cda9dd41d3cb6887e3
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2024-12-18 20:14:16 +0000

    callejon

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,29 +1,41 @@
 <?php
 
 namespace Dsw\Tema6\Models;
+use PDO;
+use PDOException;
+
 
 class User {
-  static private $users = [
-    ['id' => '1', 'name' => 'Pepe', 'surname' => 'Garcia'],
-    ['id' => '2', 'name' => 'Ana', 'surname' => 'Martinez'],
-    ['id' => '3', 'name' => 'Juan', 'surname' => 'Lopez'],
-    ['id' => '4', 'name' => 'Maria', 'surname' => 'Gonzalez'],
-    ['id' => '5', 'name' => 'Carlos', 'surname' => 'Fernandez'],
-    ['id' => '6', 'name' => 'Lucia', 'surname' => 'Rodriguez'],
-    ['id' => '7', 'name' => 'Miguel', 'surname' => 'Sanchez'],
-    ['id' => '8', 'name' => 'Laura', 'surname' => 'Perez'],
-    ['id' => '9', 'name' => 'David', 'surname' => 'Gomez'],
-    ['id' => '10', 'name' => 'Elena', 'surname' => 'Diaz']
-  ];
+
+  static private $link;
+
+  public function __construct()
+  {
+    $host = 'localhost';
+    $db = 'capasdb';
+    $user = 'root';
+    $pw = '';
+    
+    try {
+      self::$link = new PDO("mysql:host=$host;dbname=$db", $user, $pw);
+      self::$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $ex) {
+      die("Error en la conexión, mensaje de error: ". $ex->getMessage());
+    }
+   }
 
   public static function all() {
-    return self::$users;
+    $stmt = self::$link->prepare("SELECT * FROM users");
+    $stmt->execute();
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $users;
+
   }
 
   public static function get($id) {
-    return array_first(self::$users, function($user) use ($id) {
-      return $user['id'] == $id;
-    });
+    // return array_first(self::$users, function($user) use ($id) {
+    //   return $user['id'] == $id;
+    // });
   }
 }
 ?>
