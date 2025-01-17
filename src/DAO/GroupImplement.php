b9b577c75866cf71227a8fd3ccf739d74fe41ab2
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-01-17 16:39:09 +0000

    xd

--- src/DAO/GroupImplement.php
+++ src/DAO/GroupImplement.php
@@ -106,6 +106,4 @@ class GroupImplement {
       $stmt->execute();
     }
   }
-
-
 }
--- src/routers/group.php
+++ src/routers/group.php
@@ -7,5 +7,5 @@ $router->map('DELETE', '/group/[i:id]', 'GroupController#delete');
 $router->map('GET', '/group/[i:id]/edit', 'GroupController#edit');
 $router->map('PUT', '/group/[i:id]', 'GroupController#put');
 $router->map('GET', '/group/[i:id]/users', 'GroupController#users');
-$router->map('POST', '/group/[i:id]/users/', 'GroupController#postusers');
+$router->map('POST', '/group/[i:id]/users', 'GroupController#postusers');
 $router->map('GET', '/group/[i:id]/users/[i:name]', 'GroupController#users2');
