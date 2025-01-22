980a44e6f2c1f97449ffaee21ea4f093ea0b43c7
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-01-22 12:35:31 +0000

    si

--- src/DAO/GroupImplement.php
+++ src/DAO/GroupImplement.php
@@ -6,7 +6,8 @@ use Dsw\Tema6\Database\Database;
 use Dsw\Tema6\Models\Group;
 use PDO;
 
-class GroupImplement {
+class GroupImplement
+{
   private Database $db;
 
   public function __construct()
@@ -14,15 +15,15 @@ class GroupImplement {
     $this->db = new Database();
   }
 
-  public function findAll(): array 
+  public function findAll(): array
   {
     $query = 'SELECT * FROM `groups`';
     $stmt = $this->db->getConnection()->prepare($query);
     $stmt->execute();
     $groups = [];
-    while( $groupRecord = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    while ($groupRecord = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $group = new Group(
-        $groupRecord['id'], 
+        $groupRecord['id'],
         $groupRecord['name']
       );
       $groups[] = $group;
@@ -41,43 +42,47 @@ class GroupImplement {
       return null;
     } else {
       return new Group(
-        $groupRecord['id'], 
+        $groupRecord['id'],
         $groupRecord['name'],
       );
     }
   }
 
-  public function create(string $name) {
+  public function create(string $name)
+  {
     $query = "INSERT INTO `groups` (name) VALUES (:name)";
     $stmt = $this->db->getConnection()->prepare($query);
-    $stmt->bindParam(':name',$name);
+    $stmt->bindParam(':name', $name);
     $stmt->execute();
   }
 
-  public function delete(int $id) {
+  public function delete(int $id)
+  {
     $query = "DELETE FROM `groups` WHERE id = :id";
     $stmt = $this->db->getConnection()->prepare($query);
     $stmt->bindParam(':id', $id);
     $stmt->execute();
   }
 
-  public function update(int $id, string $name) {
+  public function update(int $id, string $name)
+  {
     $query = "UPDATE `groups` SET name=:name WHERE id = :id";
     $stmt = $this->db->getConnection()->prepare($query);
     $stmt->bindParam(':id', $id);
-    $stmt->bindParam(':name',$name);
+    $stmt->bindParam(':name', $name);
     $stmt->execute();
   }
 
-  public function findGroupsByUserId (int $id) {
+  public function findGroupsByUserId(int $id)
+  {
     $query = 'SELECT id, name FROM `groups` INNER JOIN group_user ON groups.id = group_user.id_group WHERE group_user.id_user = :id';
     $stmt = $this->db->getConnection()->prepare($query);
     $stmt->bindParam(':id', $id);
     $stmt->execute();
     $groups = [];
-    while( $groupRecord = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    while ($groupRecord = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $group = new Group(
-        $groupRecord['id'], 
+        $groupRecord['id'],
         $groupRecord['name']
       );
       $groups[] = $group;
@@ -85,7 +90,8 @@ class GroupImplement {
     return $groups;
   }
 
-  public function deleteUsers(array $id_users, $id_group) {
+  public function deleteUsers(array $id_users, $id_group)
+  {
     $id_user = null;
     $query = 'DELETE FROM group_user WHERE id_user = :id_user AND id_group = :id_group';
     $stmt = $this->db->getConnection()->prepare($query);
@@ -96,7 +102,8 @@ class GroupImplement {
     }
   }
 
-  public function insertUsers(array $id_users, $id_group) {
+  public function insertUsers(array $id_users, $id_group)
+  {
     $id_user = null;
     $query = 'INSERT INTO group_user (id_user, id_group) VALUES (:id_user, :id_group)';
     $stmt = $this->db->getConnection()->prepare($query);
